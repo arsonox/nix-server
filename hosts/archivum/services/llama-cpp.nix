5dfa4bab29da35c08fffdf1593a1f28613f1089f
Nox <1620870+arsonox@users.noreply.github.com>
2026-08-20 12:24:03 +0000

    Update llama-cpp.nix

--- hosts/archivum/services/llama-cpp.nix
+++ hosts/archivum/services/llama-cpp.nix
@@ -10,8 +10,8 @@
     modelsDir = "/mnt/tank/models";
     host = "127.0.0.1";
     port = 8084;
-    "n-gpu-layers" = 99;
-    "ctx-size" = 8192;
+    n-gpu-layers = 99;
+    ctx-size = 8192;
     mlock = true;
   };
 
