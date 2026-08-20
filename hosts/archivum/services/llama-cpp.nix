5a7b27a20107981ebe41d1b46ea0bc8202d54e69
Nox <1620870+arsonox@users.noreply.github.com>
2026-08-20 12:26:27 +0000

    Update llama-cpp.nix

--- hosts/archivum/services/llama-cpp.nix
+++ hosts/archivum/services/llama-cpp.nix
@@ -7,12 +7,14 @@
   services.llama-cpp = {
     enable = true;
     package = pkgs.llama-cpp-vulkan;
-    modelsDir = "/mnt/tank/models";
-    host = "127.0.0.1";
-    port = 8084;
-    n-gpu-layers = 99;
-    ctx-size = 8192;
-    mlock = true;
+    settings = {
+      modelsDir = "/mnt/tank/models";
+      host = "127.0.0.1";
+      port = 8084;
+      n-gpu-layers = 99;
+      ctx-size = 8192;
+      mlock = true;
+    };
   };
 
   systemd.tmpfiles.rules = [
